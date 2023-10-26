314839ec971027371caa1c569a85ea2611c2ad75
yo8088 <yohhankumarasinghe@Yohhans-Air.attlocal.net>
2023-10-25 21:46:45 -0400

    Fixed code to actually change the background for category.png

--- category_script.R
+++ category_script.R
@@ -4,5 +4,5 @@ ggplot(youtube, aes(x = category, fill = category)) +
   geom_histogram(stat = "count") + 
   labs(title = "Number of Channels by Channel Category", xlab = "Country", ylab = "Number of Channels") +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.background = element_rect(fill = "white"))
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),  plot.background = element_rect(fill = "white"))
 ggsave("category.png", width = 12, height = 8)
