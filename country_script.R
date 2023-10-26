23b20e81fe501d327ae40262cba0a0327233d808
yo8088 <yohhankumarasinghe@Yohhans-Air.attlocal.net>
2023-10-25 21:43:21 -0400

    Adjusted scripts and recreated plots to make the backgrounds white

--- country_script.R
+++ country_script.R
@@ -4,5 +4,5 @@ ggplot(youtube, aes(x = Country, fill = Country)) +
   geom_histogram(stat = "count") + 
   labs(title = "Number of Channels by Country", xlab = "Country", ylab = "Number of Channels") +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),  plot.background = element_rect(fill = "white"))
 ggsave("country.png", width = 12, height = 8)
