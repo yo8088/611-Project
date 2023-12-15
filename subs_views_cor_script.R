b47bac431caa1a3c66167f40be61820eeab74713
yo8088 <yohhankumarasinghe@Yohhans-Air.attlocal.net>
2023-12-15 15:23:35 -0500

    Made subs-views correlation figure and fixed associated bugs in R script

--- subs_views_cor_script.R
+++ subs_views_cor_script.R
@@ -4,9 +4,10 @@ ggplot(youtube, aes(subscribers, video.views)) +
   geom_point(aes(color = subscribers)) +
   labs(x = "Subscribers", y = "Video Views")
 channels_only<- subset(youtube, video.views != 0)
-nonzero$log_video.views<- log(nonzero$video.views)
-nonzero$log_subscribers<- log(nonzero$subscribers)
-ggplot(nonzero, aes(log_subscribers, log_video.views)) +
+channels_only$log_video.views<- log(channels_only$video.views)
+channels_only$log_subscribers<- log(channels_only$subscribers)
+channels_only<- subset(channels_only, log_video.views > 15) #Remove outliers
+ggplot(channels_only, aes(log_subscribers, log_video.views)) +
   geom_point(aes(color = video.views)) +
   labs(x = "Log Subscribers", y = "Log Video Views")
 ggsave("Figures/subs_views_cor.png", width = 12, height = 8)
