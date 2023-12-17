510f838bdb3f5b1ca28f247298b9daac6b6f0a1c
yo8088 <yohhankumarasinghe@yohhans-air.mynetworksettings.com>
2023-12-17 16:34:24 -0500

    Made minor changes to bubble plot

--- bubble.R
+++ bubble.R
@@ -7,7 +7,7 @@ channels_only<- subset(youtube, video.views != 0)
 channels_only$log_video.views<- log(channels_only$video.views)
 channels_only$log_subscribers<- log(channels_only$subscribers)
 channels_only<- subset(channels_only, log_video.views > 15) #Remove outliers
-ggplot(data = nonzero, aes(x = log_subscribers, y = log_video.views, size = uploads)) +
+ggplot(data = channels_only, aes(x = log_subscribers, y = log_video.views, size = uploads)) +
   geom_point(alpha = 0.2, color = "blue") +
   scale_size_continuous(range = c(3, 10))
 ggsave("Figures/bubble.png", width = 12, height = 8)
